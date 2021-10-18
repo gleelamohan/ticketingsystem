dbb5a98e8c122e1965e787f6b4f9a383b5becfea
Leela Mohan Gongunta <lmohan@lmohan-ltm5vno.internal.salesforce.com>
2021-10-18 21:13:19 +0530

    changes

--- app.js
+++ app.js
@@ -3,31 +3,15 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
+
 const {
-	Pool
+	Client
 } = require('pg');
-const connString =
-	"postgres://kizzscuwlypsds:975cd2db81d87998b1e0c2b9160e96de27a33948bf165bfad23522e3942db793@ec2-52-6-29-180.compute-1.amazonaws.com:5432/deeur13jtk9q5e";
-
-const pool = new Pool({
-	user: 'kizzscuwlypsds',
-	host: 'ec2-52-6-29-180.compute-1.amazonaws.com',
-	database: 'deeur13jtk9q5e',
-	password: '975cd2db81d87998b1e0c2b9160e96de27a33948bf165bfad23522e3942db793',
-	port: 5432
+const connectionString = 'postgres://kizzscuwlypsds:975cd2db81d87998b1e0c2b9160e96de27a33948bf165bfad23522e3942db793@ec2-52-6-29-180.compute-1.amazonaws.com:5432/deeur13jtk9q5e';
+const client = new Client({
+	connectionString: connectionString
 });
-
-pool.connect();
-
-const query = `SELECT * from CASES`;
-pool.query(query, (err, res) => {
-	if (err) {
-		console.error(err);
-		return;
-	}
-	console.log('Connection successful');
-	pool.end()
-});
+client.connect();
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({
@@ -43,9 +27,14 @@ app.get('/', function (req, res) {
 
 app.get('/result', function (req, res) {
 
+	client.query('SELECT * FROM CASES', function (err, result) {
+		if (err) {
+			console.log(err);
+			res.status(400).send(err);
+		}
+		res.status(200).send(result.rows);
+	});
 
-
-	res.json('Test');
 });
 app.get('/nosupport', function (req, res) {
 	res.sendFile(path.join(__dirname + '/nosupport.html'));

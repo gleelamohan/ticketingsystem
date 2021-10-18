6e25efda324f404d44ea9da9e842b0f7ed984e87
Leela Mohan Gongunta <lmohan@lmohan-ltm5vno.internal.salesforce.com>
2021-10-18 16:43:55 +0530

    changes

--- server/db.js
+++ server/db.js
@@ -39,7 +39,7 @@ const fetchMessages = () => {
         .then((res) => {
           client.release();
 
-          return parseMessages(res);
+          return res;
         })
         .catch((err) => {
           client.release();
